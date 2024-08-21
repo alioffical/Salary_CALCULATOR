10b65dcb87c84a6c2707ce45204cf6dbff2ec037
Danial Azhar <danish23021997three@gmail.com>
2024-08-21 17:32:51 +0500

    Add project files.

---
+++ Salary_CALCULATOR/Controllers/ChangeValueController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Salary_CALCULATOR.Controllers
+{
+    public class ChangeValueController : Controller
+    {
+        [HttpGet]
+        public IActionResult ChangeValue()
+        {
+            return View();
+        }
+    }
+}
---
+++ Salary_CALCULATOR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Salary_CALCULATOR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
