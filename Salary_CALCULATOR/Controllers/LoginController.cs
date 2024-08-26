22d5c835eb2a051fa9f1e9749c2a3a325092cf4e
Danial Azhar <danish23021997three@gmail.com>
2024-08-26 13:27:12 +0500

    Added dynamic value

---
+++ Salary_CALCULATOR/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Salary_CALCULATOR.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "Fotan" && password == "Fotanistan")
+            {
+                return RedirectToAction("ChangeValue");
+            }
+            else
+            {
+                // Optionally, add logic to display an error message or handle incorrect credentials
+                ViewBag.ErrorMessage = "Invalid username or password.";
+                return View();
+            }
+        }
+    }
+}
